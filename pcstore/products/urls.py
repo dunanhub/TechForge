dc6dac023568a560c760c8a6eb00927efa4059e4
LINESKL <priveth06@gmail.com>
2025-02-28 18:55:35 +0500

    fix 'ссылки на сайте'

--- pcstore/products/urls.py
+++ pcstore/products/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('categories/', views.category_list, name='category_list'),
-    path('categories/<int:category_id>/', views.subcategory_by_category, name='subcategory_by_category'),
-    path('categories/<int:category_id>/<int:subcategory_id>/', views.product_list, name='product_list'),
-    path('categories/<int:category_id>/<int:subcategory_id>/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('', views.category_list, name='category_list'),
+    path('<int:category_id>/', views.subcategory_by_category, name='subcategory_by_category'),
+    path('<int:category_id>/<int:subcategory_id>/', views.product_list, name='product_list'),
+    path('<int:category_id>/<int:subcategory_id>/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
