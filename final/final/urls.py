bbbe1800fac419280225bb0d60aa7a812f6876fa
Beka <ubniyev.b@gmail.com>
2025-04-16 17:23:07 +0500

    Send to Aliar

--- final/final/urls.py
+++ final/final/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('dashboard/', include('adminDashboard.urls')),
     path('auth/', include('auth_app.urls')),
     path('', include("shop.urls")),
     path('accounts/', include('allauth.urls')),
